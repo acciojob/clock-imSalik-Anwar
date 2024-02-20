9c961c61d96c0b40fb259fcaefb564938b483b47
Salik Anwar <135861792+imSalik-Anwar@users.noreply.github.com>
2024-02-20 23:14:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -10,16 +10,11 @@ function clock(){
 	let minutes = date.getMinutes()
 	let seconds = date.getSeconds()
 
-	if(minutes < 10){
-	  minutes = "0"+minutes
-	}
-	if(seconds < 10){
-	  seconds = "0"+seconds
-	}
+	if(minutes < 10) minutes = "0"+minutes
+	if(seconds < 10) seconds = "0"+seconds
 	let m = ""
-	if(hours < 12){
-	  m = "AM"
-	} else {
+	if(hours < 12) m = "AM"
+	else {
 	  m = "PM"
 	  hours = hours - 12
 	}
