f2a805a27f33dfa225b0d18183aae1db3b998fab
Salik Anwar <135861792+imSalik-Anwar@users.noreply.github.com>
2024-02-20 22:54:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
-//your JS code here. If required.
+let time = document.getElementById("timer")
+setInterval(clock, 1000)
+
+function clock(){
+	let date = new Date()
+	let month = date.getMonth()
+	let day = date.getDate()
+	let year = date.getFullYear()
+	let hours = date.getHours()
+	let minutes = date.getMinutes()
+	let seconds = date.getSeconds()
+
+	if(minutes < 10){
+	  minutes = "0"+minutes
+	}
+	if(seconds < 10){
+	  seconds = "0"+seconds
+	}
+	let m = ""
+	if(hours < 12){
+	  m = "AM"
+	} else {
+	  m = "PM"
+	  hours = hours - 12
+	}
+	
+	timer.innerHTML = `${month}/${day}/${year}, ${hours}:${minutes}:${seconds} ${m}`
+}
